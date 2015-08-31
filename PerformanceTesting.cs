d35acadf35c58eb3e6f0851177dc5b73b10aa12f
Tom Postler <tom@postler.me>
2015-08-31 13:38:18 -0500

    PT can now compare two implementations in the same run.

--- PerformanceTesting.cs
+++ PerformanceTesting.cs
@@ -8,7 +8,15 @@ class PerformanceTesting
     /// <summary>
     /// Edit this function to perform the perf test
     /// </summary>
-    private static void DoTheThing()
+    private static void DoTheThing0()
+    {
+        throw new NotImplementedException();
+    }
+
+    /// <summary>
+    /// Optionally define this one to do a comparison perf test
+    /// </summary>
+    private static void DoTheThing1()
     {
         throw new NotImplementedException();
     }
@@ -54,68 +62,93 @@ class PerformanceTesting
         long nanosecPerTick = (1000L * 1000L * 1000L) / Stopwatch.Frequency;
         const long numIterations = 10000;
 
-        // Define variables for operation statistics.
-        long numTicks = 0;
-        long numRollovers = 0;
-        long maxTicks = 0;
-        long minTicks = Int64.MaxValue;
-        int indexFastest = -1;
-        int indexSlowest = -1;
-        long milliSec = 0;
-
-        Stopwatch time10kOperations = Stopwatch.StartNew();
-
-        // Run the current operation 10001 times. 
-        // The first execution time will be tossed out, since it can skew the average time.
-        for (int i = 0; i <= numIterations; i++)
+        for (int whichDoTheThing = 0; whichDoTheThing < 2; whichDoTheThing++)
         {
-            long ticksThisTime = 0;
-            Stopwatch timePerParse;
-
-            timePerParse = Stopwatch.StartNew();
-
-            // Do the thing
-            DoTheThing();
-
-            timePerParse.Stop();
-            ticksThisTime = timePerParse.ElapsedTicks;
-
-            // Skip over the time for the first operation, just in case it caused a one-time performance hit.
-            if (i == 0)
+            if (whichDoTheThing == 1)
             {
-                time10kOperations.Reset();
-                time10kOperations.Start();
-            }
-            else
-            {
-                // Update operation statistics for iterations 1-10001. 
-                if (maxTicks < ticksThisTime)
+                try
                 {
-                    indexSlowest = i;
-                    maxTicks = ticksThisTime;
+                    DoTheThing1();
                 }
-                if (minTicks > ticksThisTime)
+                catch (NotImplementedException)
                 {
-                    indexFastest = i;
-                    minTicks = ticksThisTime;
-                }
-                numTicks += ticksThisTime;
-                if (numTicks < ticksThisTime)
-                {
-                    // Keep track of rollovers.
-                    numRollovers++;
+                    return;
                 }
             }
+
+            // Define variables for operation statistics.
+            long numTicks = 0;
+            long numRollovers = 0;
+            long maxTicks = 0;
+            long minTicks = Int64.MaxValue;
+            int indexFastest = -1;
+            int indexSlowest = -1;
+            long milliSec = 0;
+
+            Stopwatch time10kOperations = Stopwatch.StartNew();
+
+            // Run the current operation 10001 times. 
+            // The first execution time will be tossed out, since it can skew the average time.
+            for (int i = 0; i <= numIterations; i++)
+            {
+                long ticksThisTime = 0;
+                Stopwatch timePerParse;
+
+                switch (whichDoTheThing)
+                {
+                    case 1:
+                        timePerParse = Stopwatch.StartNew();
+                        DoTheThing1();
+                        timePerParse.Stop();
+                        break;
+
+                    case 0:
+                    default:
+                        timePerParse = Stopwatch.StartNew();
+                        DoTheThing0();
+                        timePerParse.Stop();
+                        break;
+                }
+                ticksThisTime = timePerParse.ElapsedTicks;
+
+                // Skip over the time for the first operation, just in case it caused a one-time performance hit.
+                if (i == 0)
+                {
+                    time10kOperations.Reset();
+                    time10kOperations.Start();
+                }
+                else
+                {
+                    // Update operation statistics for iterations 1-10001. 
+                    if (maxTicks < ticksThisTime)
+                    {
+                        indexSlowest = i;
+                        maxTicks = ticksThisTime;
+                    }
+                    if (minTicks > ticksThisTime)
+                    {
+                        indexFastest = i;
+                        minTicks = ticksThisTime;
+                    }
+                    numTicks += ticksThisTime;
+                    if (numTicks < ticksThisTime)
+                    {
+                        // Keep track of rollovers.
+                        numRollovers++;
+                    }
+                }
+            }
+
+            // Display the statistics for 10000 iterations.
+            time10kOperations.Stop();
+            milliSec = time10kOperations.ElapsedMilliseconds;
+
+            Console.WriteLine("Statistics for DoTheThing{0}:", whichDoTheThing);
+            Console.WriteLine("  Slowest time:  Iteration {0,4} = {1,4} ticks -> {2,6} nanoseconds", indexSlowest, maxTicks, maxTicks * nanosecPerTick);
+            Console.WriteLine("  Fastest time:  Iteration {0,4} = {1,4} ticks -> {2,6} nanoseconds", indexFastest, minTicks, minTicks * nanosecPerTick);
+            Console.WriteLine("  Average time:  {0} ticks = {1} nanoseconds", numTicks / numIterations, (numTicks * nanosecPerTick) / numIterations);
+            Console.WriteLine("  Total time looping through {0} operations: {1} milliseconds", numIterations, milliSec);
+            Console.WriteLine();
         }
-
-        // Display the statistics for 10000 iterations.
-        time10kOperations.Stop();
-        milliSec = time10kOperations.ElapsedMilliseconds;
-
-        Console.WriteLine("Statistics:");
-        Console.WriteLine("  Slowest time:  Iteration {0} = {1,4} ticks = {2,4} nanoseconds", indexSlowest, maxTicks, maxTicks * nanosecPerTick);
-        Console.WriteLine("  Fastest time:  Iteration {0} = {1,4} ticks = {2,4} nanoseconds", indexFastest, minTicks, minTicks * nanosecPerTick);
-        Console.WriteLine("  Average time:  {0} ticks = {1} nanoseconds", numTicks / numIterations, (numTicks * nanosecPerTick) / numIterations);
-        Console.WriteLine("  Total time looping through {0} operations: {1} milliseconds", numIterations, milliSec);
     }
 }
